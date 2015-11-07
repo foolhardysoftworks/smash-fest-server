38d912f71aeae48cd2412ab93d9f7712b912e337
James Dalessio <jrd261@gmail.com>
2015-11-06 21:23:04 -0500

    import

---
+++ server.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const socketIo = require('socket.io');
+const port = 8080;
+const server = http.createServer();
+server.listen(port);
+const io = socketIo(server);
+
+const sockets = {};
+const players = {};
+
+io.sockets.on('connection', function (socket) {
+  const playerId = socket.handshake.query.playerId;
+  if (!playerId) return socket.disconnect();
+  socket.playerId = playerId;
+  players[playerId] = { state: {}, id: playerId };
+  
+  socket.on('disconnect', () => {
+    delete sockets[socket.playerId];
+    delete players[socket.playerId];
+    io.emit('offline', socket.playerId);
+  });
+
+  socket.on('update', state => {
+    if (!players[socket.playerId]) return;
+    players[socket.playerId].state = state;
+    io.emit('update', { id: socket.playerId, state: state });
+  });
+
+  socket.on('hit', hit => {
+    if (!sockets[hit.target]) return;
+    sockets[hit.target].emit('hit', hit);
+  });
+
+  io.emit('online', socket.playerId);
+  socket.emit('sync', players);
+
+  return null;
+
+});
+
+
+
+
